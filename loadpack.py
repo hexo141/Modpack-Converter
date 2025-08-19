8372339d93fcc4ddf6d515550e723f9c02f0e42a
hexo141 <1337075411@qq.com>
2025-08-19 16:03:43 +0800

    Changes to be committed: 	modified:   loadpack.py 	modified:   main.py

--- loadpack.py
+++ loadpack.py
@@ -1,1 +1,28 @@
 import requests
+import pathlib
+import zipfile
+import os
+import concurrent.futures
+response = requests.get("https://staging-api.modrinth.com/",timeout=20)
+search_pool = []
+if not os.path.exists("temp"):
+    os.makedirs("temp")
+def loadpack(path):
+    with zipfile.ZipFile(path, 'r') as zip_ref:
+        zip_ref.extractall(pathlib.Path("temp") / path)
+    mods_path = pathlib.Path("temp") / path / "overrides"/"mods"
+    for mod in mods_path.iterdir():
+        search = requests.get(f"https://api.modrinth.com/v2/search?query={mod}")
+        search_pool.append(search)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        executor.map(loadpack, search_pool)
+    for search in search_pool:
+        print(search.json())
+    
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,2 @@
+import loadpack
+loadpack.loadpack(input("path: "))
