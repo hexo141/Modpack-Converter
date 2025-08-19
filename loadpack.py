0e9d72e9220ef5b84fe996e114e2a4467f461b77
hexo141 <1337075411@qq.com>
2025-08-19 16:10:32 +0800

    fix some bugs

--- loadpack.py
+++ loadpack.py
@@ -3,16 +3,15 @@ import pathlib
 import zipfile
 import os
 import concurrent.futures
-response = requests.get("https://api.modrinth.com/",timeout=20)
 search_pool = []
 if not os.path.exists("temp"):
     os.makedirs("temp")
 def loadpack(path):
     with zipfile.ZipFile(path, 'r') as zip_ref:
-        zip_ref.extractall(pathlib.Path("temp") / path)
-    mods_path = pathlib.Path("temp") / path / "overrides"/"mods"
+        zip_ref.extractall("temp")
+    mods_path = pathlib.Path("temp") / "overrides" /"mods"
     for mod in mods_path.iterdir():
-        search = requests.get(f"https://api.modrinth.com/v2/search?query={mod}")
+        search = requests.get(f"https://api.modrinth.com/v2/search?query={mod}",timeout=20)
         search_pool.append(search)
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
         executor.map(loadpack, search_pool)
