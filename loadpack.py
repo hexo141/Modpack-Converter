14fc5149b35e5c2428b964ad5f3b7d71cb193b54
hexo141 <1337075411@qq.com>
2025-08-19 16:44:46 +0800

    fix get error

--- loadpack.py
+++ loadpack.py
@@ -3,16 +3,36 @@ import pathlib
 import zipfile
 import os
 import concurrent.futures
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
 search_pool = []
 if not os.path.exists("temp"):
     os.makedirs("temp")
 
 def search_mod(mod_name):
-    search = requests.get(f"https://api.modrinth.com/v2/search?query={mod_name}",timeout=20,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"})
+    session = requests.Session()
+    retry = Retry(
+        total=10,
+        read=10,
+        connect=10,
+        backoff_factor=0.1,
+        status_forcelist=[500, 502, 503, 504],
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    
+    # 增加超时时间
+    session.timeout = 20
+    
+    search = session.get(f"https://api.modrinth.com/v2/search?query={mod_name}",timeout=20,headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"})
     return search
 def loadpack(path):
     with zipfile.ZipFile(path, 'r') as zip_ref:
-        zip_ref.extractall("temp")
+        for filename in zip_ref.namelist():
+            print(f"Extracting {filename} to temp")
+            zip_ref.extract(filename, "temp")
     mods_path = pathlib.Path("temp") / "overrides" /"mods"
     mod_names = []
     for mod in mods_path.iterdir():
